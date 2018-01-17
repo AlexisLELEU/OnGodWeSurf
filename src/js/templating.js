c30e8d7b0c19b7ecf92b86a9077b4721364b6197
Kola-Kola <jonathan.ibor@hetic.net>
2018-01-17 16:12:14 +0100

    [FEATURE] HANDLEBARS TEMPLATING

--- src/js/templating.js
+++ src/js/templating.js
@@ -1,12 +1,39 @@
+import Handlebars from 'handlebars'
+
 export default class Templating {
 
     constructor() {
         this._grabDom();
         this._addListener();
+
+        this._getData();
+        this._putData();
     }
 
 
     /* PRIVATE METHODS */
+
+    _createBounds() {
+        ['_getData', '_putData']
+            .forEach((fn) => this[fn] = this[fn].bind(this));
+    }
+
+    _putData () {
+        for (let i = 0; i < this._parsing.search.length; i++) {
+            let compiledTemplate = Handlebars.compile(this._dom.cardsTemplate);
+            let generated = compiledTemplate(this._parsing.search[i]);
+            this._dom.cardsContainer.innerHTML += generated
+        }
+    }
+
+    _getData () {
+        let req = new XMLHttpRequest();
+        req.open('GET', 'http://joibor.fr/api/search.json', false);
+        req.send(null);
+        if (req.status === 200) {
+            return this._parsing = JSON.parse(req.responseText);
+        }
+    }
 
     /* END PRIVATE METHODS */
 
@@ -25,7 +52,9 @@ export default class Templating {
     /* GRAB DOM */
 
     _grabDom () {
-        this._dom = {}
+        this._dom = {};
+        this._dom.cardsTemplate = document.querySelector('#cards-template').innerHTML;
+        this._dom.cardsContainer = document.querySelector('.section-bottom__template');
     }
 
     /* END GRAB DOM*/
